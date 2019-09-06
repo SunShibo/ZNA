f87ac78364b3bf839d4a4006ae46e0d1f9ca8e4c
xiaowen1024 <15214440324@163.com>
2019-09-06 14:17:02 +0800

    sdaf

--- src/main/java/com/zna/server/entity/bo/ProjectTypeNameBO.java
+++ src/main/java/com/zna/server/entity/bo/ProjectTypeNameBO.java
@@ -5,17 +5,8 @@ import com.zna.server.common.base.BaseModel;
 import java.util.List;
 
 public class ProjectTypeNameBO extends BaseModel {
-    private String id;
     private String label;
     private List<ProjectListBO> children;//展览
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public String getLabel() {
         return label;
