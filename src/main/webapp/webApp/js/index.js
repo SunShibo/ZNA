67b936bb1126676ea3ab0f5c711ac285778866c2
hp <475341496@qq.com>
2019-09-23 17:49:23 +0800

    ZNA移动端页面修改BUG

--- src/main/webapp/webApp/js/index.js
+++ src/main/webapp/webApp/js/index.js
@@ -36,287 +36,6 @@
         }
     ];
 
-    //全局上声明一个切换中英文的方法
-    window.switch_language = new Function();
-
-    $(function () {
-
-        //跳转
-        var clientWidth = document.body.clientWidth;
-        if (clientWidth > 850) {
-            $.checkPlatform();
-        }
-
-        //获取语言类型
-        lang = $.getUrl('language') === 'en';
-        if (lang) {
-            langDom.text('中');
-            $('.logo').removeClass('ch_logo_w').addClass('en_logo_w');
-        } else {
-            langDom.text('EN');
-            $('.logo').removeClass('en_logo_w').addClass('ch_logo_w');
-        }
-
-        //LOGO
-        setLogo(lang);
-
-
-        if (location.href.indexOf('?') === -1) {
-            location.href = location.href + "?language=" + $.getLangStr();
-        }
-        //设置菜单
-        settingMenu(lang);
-        //滑动事件的优化
-        stopTouchendPropagationAfterScroll();
-        //执行回调
-        window.switch_language(lang);
-    });
-
-    //按语言设置菜单
-    function settingMenu(lang) {
-
-        //设置logo路径
-        var logoDom = $('.logo>a');
-        logoDom.attr('href', 'home.html' + '?language=' + $.getLangStr());
-
-        //遍历菜单
-        for (var i = 0, len = menu.length; i < len; i++) {
-            var dom = $('.nav-ul').children('li').eq(i);
-            var aSrc = dom.children('a').attr('href');
-            //设置一级菜单
-            if (lang) {
-                dom.children('a').text(menu[i].nameEN).attr('href', aSrc + '?language=' + $.getLangStr());
-            } else {
-                dom.children('a').text(menu[i].name).attr('href', aSrc + '?language=' + $.getLangStr());
-            }
-            //设置二级菜单
-            if (menu[i].children.length) {
-                for (var j = 0, jLen = menu[i].children.length; j < jLen; j++) {
-                    var subDom = dom.find('.sub-nav').children('li').eq(j).children('a');
-                    var subSrc = subDom.attr('href');
-                    subSrc = subSrc.indexOf('?') === -1 ? subSrc + '?' : subSrc + '&';
-                    if (lang) {
-                        subDom.text(menu[i].children[j].nameEN).attr('href', subSrc + 'language=' + $.getLangStr());
-                    } else {
-                        subDom.text(menu[i].children[j].name).attr('href', subSrc + 'language=' + $.getLangStr());
-                    }
-                }
-            }
-        }
-    }
-
-    /**
-     * 滑动优化
-     * 有轮播的页面不进入此事件优化
-     */
-    function stopTouchendPropagationAfterScroll() {
-
-        var locked = false;
-
-        if (!(window.location.pathname.indexOf('home')) || !(window.location.pathname.indexOf('aboutUs'))) {
-
-            window.addEventListener('touchmove', function (ev) {
-
-                locked || (locked = true, window.addEventListener('touchend', stopTouchendPropagation, true));
-
-            }, true);
-
-            function stopTouchendPropagation(ev) {
-
-                ev.stopPropagation();
-
-                window.removeEventListener('touchend', stopTouchendPropagation, true);
-
-                locked = false;
-
-            }
-
-        }
-
-    }
-
-
-    //设置语音
-    langDom.on('touchend', function (e) {
-        //更改当前页面的语言变量
-        lang = !lang;
-        //设置logo
-        setLogo(lang);
-
-
-        //保存语言类型
-        localStorage.setItem('EN_LANG', lang ? 'EN_US' : 'EN_CH');
-        //更新菜单
-        settingMenu(lang);
-        if (lang) {
-            langDom.text('中');
-            $('.logo').removeClass('ch_logo_w').addClass('en_logo_w');
-        } else {
-            langDom.text('EN');
-            $('.logo').removeClass('en_logo_w').addClass('ch_logo_w');
-        }
-        //触发并更新回调参数
-        window.switch_language(lang);
-    });
-
-    //根据语言设置logo
-    function setLogo(lang) {
-        //LOGO
-        var imageUrl = lang ? 'image/logo.jpg' : 'image/logo_cn.jpg';
-        $('#logoImage').attr('src', imageUrl);
-    }
-
-    //菜单显示
-    $('#clickMenu').on('touchend', function () {
-        menuState(m);
-    });
-
-    //菜单蒙层和关闭按钮
-    navDom.on('touchend', function (e) {
-        var ev = ev || window.event;
-        var target = ev.target || ev.srcElement;
-        //防止点击菜单也关闭，只有点击最外层的div#nav的时候才关闭
-        if (m && (target.id === 'nav' || target.id === 'closeMenu' || target.nodeName.toLowerCase() === 'img')) {
-            menuState(true);
-            m = false;
-        } else if (s && (target.id === 'closeMenu' || target.nodeName.toLowerCase() === 'img')) {
-            searchState(true);
-            s = false;
-        }
-    });
-
-    //搜索
-    $('#clickSearch').on('touchend', function (e) {
-        searchState(s);
-    });
-
-    //搜索框显示
-    function searchState(state) {
-        var search = $('.head_search, .search_list');
-        $('#keyword').val('');
-        if (state) {
-            search.fadeOut();
-            topHeadState(state);
-            s = false;
-        } else {
-            search.fadeIn();
-            topHeadState(state);
-            for (var i = 0, len = search_menu.length; i < len; i++) {
-                $('.search_list .list div').eq(i).children('a').text($.getLang() ? search_menu[i].nameEN : search_menu[i].name);
-            }
-            s = true;
-        }
-    }
-
-    // 控制菜单是否显示
-    function menuState(state) {
-        if (state) {
-            navDom.fadeOut();
-            topHeadState(state);
-            //将二级菜单隐藏
-            $('.sub').hide();
-            $(document.body).css({"overflow-y": "auto"});
-            m = false;
-        } else {
-            navDom.fadeIn();
-            topHeadState(state);
-            $(document.body).css({"overflow-y": "hidden"});
-            m = true;
-        }
-    }
-
-    //控制头部按钮显示
-    function topHeadState(state) {
-        if (state) {
-            closeDom.hide();
-            openDom.show();
-        } else {
-            closeDom.show();
-            openDom.hide();
-        }
-    }
-
-    //控制子菜单是否显示
-    function subMenuState(state, that) {
-        $('.sub').hide();
-        if (state) {
-            that.children(".sub").hide();
-        } else {
-            that.children(".sub").show();
-        }
-    }
-
-    //一级菜单的事件委托
-    $('.nav-ul li').on('touchend', function (e) {
-        var ev = ev || window.event, target = ev.target || ev.srcElement, that = $(this);
-        switch (target.id) {
-            case 'about':
-                subMenuState(true, that);
-                break;
-            case 'project':
-                subMenuState(false, that);
-                break;
-            case 'team':
-                subMenuState(true, that);
-                break;
-            case 'dynamic':
-                subMenuState(true, that);
-                break;
-            case 'information':
-                subMenuState(false, that);
-                break;
-        }
-    });
-
-    //搜索页面导航
-    $('.list').on('touchend', function (ev) {
-        var ev = ev || window.event, target = ev.target || ev.srcElement;
-        var search = $('#keyword').val();
-        switch (target.id) {
-            case 'toProject':
-                location.href = 'project.html?language=' + $.getLangStr() + "&search=" + encodeURIComponent(search);
-                break;
-            case 'toNews':
-                location.href = 'news.html?language=' + $.getLangStr() + "&search=" + encodeURIComponent(search);
-                break;
-        }
-    });
-
-    //底部跳转链接
-    $('.icon-list').on('touchend', function (ev) {
-        var ev = ev || window.event, target = ev.target || ev.srcElement;
-        switch (target.id) {
-            case 'toMicroblog':
-                location.href = 'https://weibo.com/znabeijing?topnav=1&wvr=6&topsug=1&is_all=1';
-                break;
-            case 'toIn':
-                location.href = 'https://www.linkedin.com/feed/?trk=v5nav_cap_to_consumer_home';
-                break;
-            case 'toWechat':
-                $(document.body).css({"overflow-y": "hidden"});
-                $('#icon_wechat').show();
-                break;
-        }
-    });
-
-    //底部跳转icon
-    $('#icon_wechat').on('touchend', function (e) {
-        var ev = ev || window.event, target = ev.target || ev.srcElement;
-        if (target.id === 'icon_wechat') {
-            $('#icon_wechat').hide();
-            $(document.body).css({"overflow-y": "auto"});
-        }
-    });
-
-    // 页面适配
-    $(window).resize(function (ev) {
-        $('.home-item-image>div').removeClass('home-item-image-wrapper').addClass('home-item-image-wrapper');
-        var clientWidth = document.body.clientWidth;
-        if (clientWidth > 850 && !(/android/i.test(navigator.userAgent) || /(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent))) {
-            $.checkPlatform();
-        }
-    });
-
     //扩展jquery方法
     $.extend({
         baseUrl: function () {
@@ -422,4 +141,287 @@
             // }
         }
     });
+
+    //全局上声明一个切换中英文的方法
+    window.switch_language = new Function();
+
+    $(function () {
+
+        //跳转
+        var clientWidth = document.body.clientWidth;
+        if (clientWidth > 850) {
+            $.checkPlatform();
+        }
+
+        //获取语言类型
+        lang = $.getUrl('language') === 'en';
+        if (lang) {
+            langDom.text('中');
+            $('.logo').removeClass('ch_logo_w').addClass('en_logo_w');
+        } else {
+            langDom.text('EN');
+            $('.logo').removeClass('en_logo_w').addClass('ch_logo_w');
+        }
+
+        //LOGO
+        setLogo(lang);
+
+
+        if (location.href.indexOf('?') === -1) {
+            location.href = location.href + "?language=" + $.getLangStr();
+        }
+        //设置菜单
+        settingMenu(lang);
+        //滑动事件的优化
+        stopTouchendPropagationAfterScroll();
+        //执行回调
+        window.switch_language(lang);
+    });
+
+    //按语言设置菜单
+    function settingMenu(lang) {
+
+        //设置logo路径
+        var logoDom = $('.logo>a');
+        logoDom.attr('href', 'home.html' + '?language=' + $.getLangStr());
+
+        //遍历菜单
+        for (var i = 0, len = menu.length; i < len; i++) {
+            var dom = $('.nav-ul').children('li').eq(i);
+            var aSrc = dom.children('a').attr('href');
+            //设置一级菜单
+            if (lang) {
+                dom.children('a').text(menu[i].nameEN).attr('href', aSrc + '?language=' + $.getLangStr());
+            } else {
+                dom.children('a').text(menu[i].name).attr('href', aSrc + '?language=' + $.getLangStr());
+            }
+            //设置二级菜单
+            if (menu[i].children.length) {
+                for (var j = 0, jLen = menu[i].children.length; j < jLen; j++) {
+                    var subDom = dom.find('.sub-nav').children('li').eq(j).children('a');
+                    var subSrc = subDom.attr('href');
+                    subSrc = subSrc.indexOf('?') === -1 ? subSrc + '?' : subSrc + '&';
+                    if (lang) {
+                        subDom.text(menu[i].children[j].nameEN).attr('href', subSrc + 'language=' + $.getLangStr());
+                    } else {
+                        subDom.text(menu[i].children[j].name).attr('href', subSrc + 'language=' + $.getLangStr());
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 滑动优化
+     * 有轮播的页面不进入此事件优化
+     */
+    function stopTouchendPropagationAfterScroll() {
+
+        var locked = false;
+
+        if (!(window.location.pathname.indexOf('home')) || !(window.location.pathname.indexOf('aboutUs'))) {
+
+            window.addEventListener('touchmove', function (ev) {
+
+                locked || (locked = true, window.addEventListener('touchend', stopTouchendPropagation, true));
+
+            }, true);
+
+            function stopTouchendPropagation(ev) {
+
+                ev.stopPropagation();
+
+                window.removeEventListener('touchend', stopTouchendPropagation, true);
+
+                locked = false;
+
+            }
+
+        }
+
+    }
+
+
+    //设置语音
+    langDom.on('touchend click', function (e) {
+        //更改当前页面的语言变量
+        lang = !lang;
+        //设置logo
+        setLogo(lang);
+
+
+        //保存语言类型
+        localStorage.setItem('EN_LANG', lang ? 'EN_US' : 'EN_CH');
+        //更新菜单
+        settingMenu(lang);
+        if (lang) {
+            langDom.text('中');
+            $('.logo').removeClass('ch_logo_w').addClass('en_logo_w');
+        } else {
+            langDom.text('EN');
+            $('.logo').removeClass('en_logo_w').addClass('ch_logo_w');
+        }
+        //触发并更新回调参数
+        window.switch_language(lang);
+    });
+
+    //根据语言设置logo
+    function setLogo(lang) {
+        //LOGO
+        var imageUrl = lang ? 'image/logo.jpg' : 'image/logo_cn.jpg';
+        $('#logoImage').attr('src', imageUrl);
+    }
+
+    //菜单显示
+    $('#clickMenu').on('touchend click', function () {
+        menuState(m);
+    });
+
+    //菜单蒙层和关闭按钮
+    navDom.on('touchend click', function (e) {
+        var ev = ev || window.event;
+        var target = ev.target || ev.srcElement;
+        //防止点击菜单也关闭，只有点击最外层的div#nav的时候才关闭
+        if (m && (target.id === 'nav' || target.id === 'closeMenu' || target.nodeName.toLowerCase() === 'img')) {
+            menuState(true);
+            m = false;
+        } else if (s && (target.id === 'closeMenu' || target.nodeName.toLowerCase() === 'img')) {
+            searchState(true);
+            s = false;
+        }
+    });
+
+    //搜索
+    $('#clickSearch').on('touchend click', function (e) {
+        searchState(s);
+    });
+
+    //搜索框显示
+    function searchState(state) {
+        var search = $('.head_search, .search_list');
+        $('#keyword').val('');
+        if (state) {
+            search.fadeOut();
+            topHeadState(state);
+            s = false;
+        } else {
+            search.fadeIn();
+            topHeadState(state);
+            for (var i = 0, len = search_menu.length; i < len; i++) {
+                $('.search_list .list div').eq(i).children('a').text($.getLang() ? search_menu[i].nameEN : search_menu[i].name);
+            }
+            s = true;
+        }
+    }
+
+    // 控制菜单是否显示
+    function menuState(state) {
+        if (state) {
+            navDom.fadeOut();
+            topHeadState(state);
+            //将二级菜单隐藏
+            $('.sub').hide();
+            $(document.body).css({"overflow-y": "auto"});
+            m = false;
+        } else {
+            navDom.fadeIn();
+            topHeadState(state);
+            $(document.body).css({"overflow-y": "hidden"});
+            m = true;
+        }
+    }
+
+    //控制头部按钮显示
+    function topHeadState(state) {
+        if (state) {
+            closeDom.hide();
+            openDom.show();
+        } else {
+            closeDom.show();
+            openDom.hide();
+        }
+    }
+
+    //控制子菜单是否显示
+    function subMenuState(state, that) {
+        $('.sub').hide();
+        if (state) {
+            that.children(".sub").hide();
+        } else {
+            that.children(".sub").show();
+        }
+    }
+
+    //一级菜单的事件委托
+    $('.nav-ul li').on('touchend click', function (e) {
+        var ev = ev || window.event, target = ev.target || ev.srcElement, that = $(this);
+        switch (target.id) {
+            case 'about':
+                subMenuState(true, that);
+                break;
+            case 'project':
+                subMenuState(false, that);
+                break;
+            case 'team':
+                subMenuState(true, that);
+                break;
+            case 'dynamic':
+                subMenuState(true, that);
+                break;
+            case 'information':
+                subMenuState(false, that);
+                break;
+        }
+    });
+
+    //搜索页面导航
+    $('.list').on('touchend click', function (ev) {
+        var ev = ev || window.event, target = ev.target || ev.srcElement;
+        var search = $('#keyword').val();
+        switch (target.id) {
+            case 'toProject':
+                location.href = 'project.html?language=' + $.getLangStr() + "&search=" + encodeURIComponent(search);
+                break;
+            case 'toNews':
+                location.href = 'news.html?language=' + $.getLangStr() + "&search=" + encodeURIComponent(search);
+                break;
+        }
+    });
+
+    //底部跳转链接
+    $('.icon-list').on('touchend click', function (ev) {
+        var ev = ev || window.event, target = ev.target || ev.srcElement;
+        switch (target.id) {
+            case 'toMicroblog':
+                location.href = 'https://weibo.com/znabeijing?topnav=1&wvr=6&topsug=1&is_all=1';
+                break;
+            case 'toIn':
+                location.href = 'https://www.linkedin.com/feed/?trk=v5nav_cap_to_consumer_home';
+                break;
+            case 'toWechat':
+                $(document.body).css({"overflow-y": "hidden"});
+                $('#icon_wechat').show();
+                break;
+        }
+    });
+
+    //底部跳转icon
+    $('#icon_wechat').on('touchend click', function (e) {
+        var ev = ev || window.event, target = ev.target || ev.srcElement;
+        if (target.id === 'icon_wechat') {
+            $('#icon_wechat').hide();
+            $(document.body).css({"overflow-y": "auto"});
+        }
+    });
+
+    // 页面适配
+    $(window).resize(function (ev) {
+        $('.home-item-image>div').removeClass('home-item-image-wrapper').addClass('home-item-image-wrapper');
+        var clientWidth = document.body.clientWidth;
+        if (clientWidth > 850 && !(/android/i.test(navigator.userAgent) || /(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent))) {
+            $.checkPlatform();
+        }
+    });
+
+
 })(window, jQuery);
